d89001df58e5e79ee175f0805ae39dae8d75ead8
Ankit Rathore <ankitrathore@Ankits-MacBook-Air.local>
2020-10-14 00:28:41 +0530

    Updates

---
+++ precache-manifest.6e68635d9b817f6ef79c55d4214fe9ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c811534dd0a007eddcd17011461558f",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "8e9f65b1f549f053f2af",
+    "url": "/WeatherApp/static/css/main.cd53f2fa.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/WeatherApp/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9f65b1f549f053f2af",
+    "url": "/WeatherApp/static/js/main.367488c8.chunk.js"
+  },
+  {
+    "revision": "1d501c6cbc4900159aea",
+    "url": "/WeatherApp/static/js/runtime-main.46a7f6c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeatherApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
